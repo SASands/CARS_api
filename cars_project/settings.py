88ddbfff601073652bc0fefb083a27830eea75ef
SASands <snads@live.com>
2022-03-30 18:39:14 -0400

    Registered Car model with admin site + completed POST functionality for car + updated decimal settings for Rest framework

--- cars_project/settings.py
+++ cars_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from pickle import FALSE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,6 +90,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
